5b2c4adcbf8b160d974890a27cdf2cad68891053
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2024-07-30 18:41:44 +0200

    implemented mobile navbar and made home component responsive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  menuOpen: boolean = false;
+  activeSection: string = '';
+  router = inject(ActivatedRoute);
 
+  constructor() {
+    const test = this.router.url.subscribe((params) => {
+      console.log(params[0].path);
+    });
+  }
+
+  openMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
 }
