2c6bc4c3c1b4280a59028ab6c2e690345168a8b2
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2024-08-03 20:06:13 +0200

    implemented about me page, styled it and made it responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'about-me', component: AboutMeComponent },
 ];
